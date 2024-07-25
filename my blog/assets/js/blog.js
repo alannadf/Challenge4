3d8343e90d6d20c5b1b61068752e0ee5181e62d6
alannadf <alannadf@github.com>
2024-07-25 13:21:55 -0400

    Created and styled blog-post elemetn for each entry.

--- my blog/assets/js/blog.js
+++ my blog/assets/js/blog.js
@@ -5,3 +5,10 @@ document.getElementById('backButton').addEventListener('click', function() {
 const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
 const postsContainer = document.getElementById('postsContainer');
 
+blogPosts.forEach(post => {
+    const postElement = document.createElement('div');
+    postElement.classList.add('blog-post');
+
+    
+})
+
