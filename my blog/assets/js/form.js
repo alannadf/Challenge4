c0564a74edba8302058647f7804be40aee317b7e
alannadf <alannadf@github.com>
2024-07-25 13:06:53 -0400

    Fixed syntax and testing if data is correctly stored.

--- my blog/assets/js/form.js
+++ my blog/assets/js/form.js
@@ -5,8 +5,20 @@ document.getElementById('blogForm').addEventListener('submit',function(event){
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
 
+    console.log('Username:', username);
+    console.log('Title:', title);
+    console.log('Content:', content);
+
     if (!username || !title || !content) {
         alert('Please complete all fields.');
         return;
     }
+
+    const blogPost = {username, title, content};
+
+    let blogPosts= JSON.parse(localStorage.getItem('blogPosts')) || [];
+    blogPosts.push(blogPost);
+    localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+
+    window.location.href = 'blog.html';
 });
