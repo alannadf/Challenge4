bb5cb280f56cbc8cfdbaa90d5488a3308f9d5207
alannadf <alannadf@github.com>
2024-07-25 13:18:59 -0400

    Initialize blogPosts from local storage and select postsContainer element

--- my blog/assets/js/blog.js
+++ my blog/assets/js/blog.js
@@ -1,3 +1,7 @@
 document.getElementById('backButton').addEventListener('click', function() {
     window.location.href = 'index.html';
 })
+
+const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+const postsContainer = document.getElementById('postsContainer');
+
